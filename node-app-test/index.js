c13eca7eda290fe482614f9dfc8dc72b77c4b309
daehyuk1231 <daehyuk1231@naver.com>
2026-04-21 15:50:07 +0900

    ssr

--- node-app-test/index.js
+++ node-app-test/index.js
@@ -1,1 +1,21 @@
-console.log("app running...");
+// export default로 내보내기한 것을 가져오는 방법
+import moduleA from "./moduleA.js";
+
+// 변수값 읽기
+console.log(moduleA.moduleA_var1);
+console.log(moduleA.moduleA_var2);
+console.log(moduleA.moduleA_var3);
+
+// 함수 호출 코드
+moduleA.moduleA_fun1();
+moduleA.moduleA_fun2();
+moduleA.moduleA_fun3();
+
+// export로 내보내기한 것을 가져오는 방법
+import moduleB, { moduleB_var1, moduleB_fun1 } from "./moduleB.js";
+// 변수값 읽기
+console.log(moduleB_var1);
+console.log(moduleB.moduleB_var2);
+// 함수 호출
+moduleB_fun1();
+moduleB.moduleB_fun2();
